041204d5e5823489f75354d43b08c7ed963d88be
Aliaksei Aziauchykau <alaznick@gmail.com>
2019-09-24 15:05:43 +0300

    [AA] Add company endpoints

--- src/app/modules/core/pages/home/home.component.ts
+++ src/app/modules/core/pages/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AlertsMessagesService } from './../../../../shared/services';
 import { Alert, CompanyCard } from './../../../../shared/models';
 import { ItemsService } from '../../services';
+import { BaseApolloService } from '../../services-apollo/base/base.apollo.service';
+import { UserApolloService } from 'src/app/shared/services-apollo/user.apollo.service';
+import { flatMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'wd-home',
@@ -15,7 +18,8 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private readonly alertsService: AlertsMessagesService,
-    private readonly itemsService: ItemsService
+    private readonly itemsService: ItemsService,
+    private readonly userApolloService: UserApolloService,
   ) {
     this.alertsService.alerts$.subscribe((alerts: Alert[]) => {
       this.alerts = alerts
@@ -27,7 +31,18 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.itemsService.getItems('companies')
+    this.itemsService.getItems('companies');
+    this.userApolloService.getAllUsers()
+      .pipe(
+        flatMap(x => this.userApolloService.getUser(x[0].uid)),
+        tap(x => console.log('OLOLO', x))
+      ).subscribe();
+
+    this.userApolloService.getAllCompanies()
+      .pipe(
+        flatMap(x => this.userApolloService.getCompany(x[0].cid)),
+        tap(x => console.log('OLOLO 2', x))
+      ).subscribe();
   }
 
 }
