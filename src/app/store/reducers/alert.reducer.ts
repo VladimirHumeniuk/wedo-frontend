93e172b725b0c06c2a445d490fd749eb98f17a80
aliaksei_aziauchykau <aliaksei_aziauchykau@epam.com>
2020-04-30 11:24:20 +0300

    [AA] Fix splicing alerts. For some reasons, object comes to array with configurable properties in false from ngrx/store after updating packages. This is quick fix, but probably need more investigation here.

--- src/app/store/reducers/alert.reducer.ts
+++ src/app/store/reducers/alert.reducer.ts
@@ -1,4 +1,3 @@
-import { Action } from '@ngrx/store';
 import { Alert } from '../../shared/models';
 import * as AlertActions from '../actions/alert.action';
 
@@ -8,8 +7,11 @@ export function alertReducer(state: Alert[] = [], action: AlertActions.Actions) 
       return [...state, action.payload]
 
     case AlertActions.REMOVE_ALERT:
-      state.splice(action.payload, 1);
-      return [...state];
+      // For some reasons, object comes to array with configurable properties in false from ngrx/store
+      // after updating packages. This is quick fix, but probably need more investigation here.
+      const copyState = Array.from(state);
+      copyState.splice(action.payload, 1);
+      return [...copyState];
 
     default:
         return state
