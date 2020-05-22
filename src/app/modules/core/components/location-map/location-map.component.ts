3db37fe8f763254ee3880151f344cfd7b33ff833
VladimirHumeniuk <vg6962@gmail.com>
2020-05-22 16:01:05 +0200

    feat(new map)

--- src/app/modules/core/components/location-map/location-map.component.ts
+++ src/app/modules/core/components/location-map/location-map.component.ts
@@ -1,6 +1,5 @@
 import { LocationiqService } from './../../../../shared/services/locationiq.service';
 import { Component, OnInit, Input } from '@angular/core';
-import { latLng, tileLayer, marker, icon, popup, map, LatLngExpression } from 'leaflet';
 
 @Component({
   selector: 'wd-location-map',
@@ -11,7 +10,7 @@ export class LocationMapComponent implements OnInit {
 
   @Input() address: string
 
-  public latLon: LatLngExpression
+  public latLon: number[]
   public options: any
   public layers: any
 
@@ -24,30 +23,7 @@ export class LocationMapComponent implements OnInit {
       await this.locationService.searchLocation(this.address)
       .then(response => response.json())
       .then(data => {
-
-        this.latLon = [parseFloat(data[0].lat), parseFloat(data[0].lon)];
-
-        this.options = {
-          layers: [
-            tileLayer('https://{s}-tiles.locationiq.com/v2/obk/r/{z}/{x}/{y}.png?key=pk.1a7c9d618b307088211351ffdc12e5a0', {
-              attribution: 'Open Street Map',
-            })
-          ],
-          zoom: 16,
-          minZoom: 12,
-          center: latLng(this.latLon[0], this.latLon[1])
-        };
-
-        this.layers = [
-          marker(this.latLon, {
-            icon: icon({
-               iconSize: [ 25, 41 ],
-               iconAnchor: [ 13, 41 ],
-               iconUrl: 'assets/marker-icon.png',
-               shadowUrl: 'assets/marker-shadow.png'
-            }),
-         })
-        ]
+        this.latLon = [parseFloat(data[0].lon), parseFloat(data[0].lat)];
       })
     }
   }
