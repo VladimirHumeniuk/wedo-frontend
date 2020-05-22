1bf34a4e1f92df5d3cf11b31837507922aebed7d
vhmnk <vg6962@gmail.com>
2020-05-22 16:04:32 +0200

    feat(new map) (#44)

--- src/app/shared/services/locationiq.service.ts
+++ src/app/shared/services/locationiq.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
+import { locationiq } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationiqService {
 
-  private readonly token: string = 'pk.1a7c9d618b307088211351ffdc12e5a0'
   private readonly API_URL: string = `https://eu1.locationiq.com/v1/`
 
   constructor() { }
 
   public searchLocation(search_string: string) {
-    return fetch(`${this.API_URL}search.php?key=${this.token}&q=,${search_string}&countrycodes=GI&format=json`, {
+    return fetch(`${this.API_URL}search.php?key=${locationiq.accessToken}&q=,${search_string}&countrycodes=gi&format=json`, {
       method: 'GET',
       mode: 'cors',
     })
